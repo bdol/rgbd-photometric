ecbc864651998603dea138eede9c3c870f618496
Brian Dolhansky <bdol@Brians-MacBook-Pro.local>
2013-12-02 10:21:24 -0800

    added saving of rgb frames

--- rgbd-util/oni_to_frames.py
+++ rgbd-util/oni_to_frames.py
@@ -1,18 +1,26 @@
 from primesense import openni2
+import os
 import sys
 import numpy
 import cv2
 
-if len(sys.argv)<2:
-    print "Usage: python oni_to_frames.py <path to .oni>"
+if len(sys.argv)<3:
+    print "Usage: python oni_to_frames.py <path to .oni> <out_dir>"
     sys.exit(1)
+
+outDir = os.path.abspath(sys.argv[2])
+rgbDir = os.path.join(outDir, "rgb")
+depthDir = os.path.join(outDir, "depth")
+if not os.path.isdir(outDir):
+    os.mkdir(outDir)
+    os.mkdir(rgbDir)
+    os.mkdir(depthDir)
 
 niRedistPath = "openni/Redist"
 openni2.initialize(niRedistPath)
 
 fileName = sys.argv[1]
 dev = openni2.Device.open_file(fileName)
-
 
 depth_stream = dev.create_depth_stream()
 n_depth_frames = depth_stream.get_number_of_frames()
@@ -30,12 +38,16 @@ print "Saving frames..."
 rows = 240
 cols = 320
 
+c = 0
 for i in range(0, n_color_frames):
      frame = color_stream.read_frame()
      frame_data = frame.get_buffer_as_triplet()
      color = numpy.ndarray((rows, cols, 3), numpy.dtype('uint8'), frame_data)
+     fName = os.path.join(rgbDir, "rgb_"+str(c).zfill(5)+".png")
+     cv2.imwrite(fName, color)
      cv2.imshow('test', color)
      cv2.waitKey(1)
+     c += 1
 print "Saved color frames."
 
 rows = 240
@@ -45,11 +57,9 @@ depth = numpy.zeros((rows, cols))
 for i in range(0, n_depth_frames):
     frame = depth_stream.read_frame()
     frame_data = frame.get_buffer_as_uint16()
-    print len(frame_data)
     for j in range(rows):
-    	for k in range(cols):
-    		#print j*cols+k
-    		depth[j,k] = frame_data[j*cols+k]
+        for k in range(cols):
+            depth[j,k] = frame_data[j*cols+k]
     cv2.imshow('test', depth/numpy.max(depth))
     cv2.waitKey(1)
 print "Saved depth frames."
