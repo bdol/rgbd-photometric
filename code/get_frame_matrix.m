091077e506341c77f653948417a7e16bf68fd966
Brian Dolhansky <bdolmail@gmail.com>
2013-12-02 11:11:47 -0800

    updated initial SVD code, not finished yet

--- code/get_frame_matrix.m
+++ code/get_frame_matrix.m
@@ -5,19 +5,36 @@ if nargin<2
 end
 
 M = [];
+minVal = 1000; maxVal = -1;
+
 files = dir(rootFramesPath);
 t = CTimeleft(numel(files));
+
 for i=1:numel(files)
     t.timeleft();
     if ~strcmp(files(i).name(1), '.') % skip the '.' files
         f = [rootFramesPath files(i).name];
         I = imread(f);
-        I = rgb2gray(imcrop(I, cropRect));
+        I = double(rgb2gray(imcrop(I, cropRect)));
+        
+        mn = min(I(:));
+        mx = max(I(:));
+        if mn<minVal
+            minVal = mn;
+        end
+        
+        if mx>maxVal
+            maxVal = mx;
+        end
+        
         M = [M; I(:)'];
     end
     
 end
 
+% Normalize
+M = bsxfun(@minus, M, minVal);
+M = bsxfun(@rdivide, M, maxVal-minVal);
 
 
 end
--- code/test_rec_bulb_close.m
+++ code/test_rec_bulb_close.m
@@ -1,1 +1,13 @@
 M = get_frame_matrix('~/code/datasets/photometric/bulb_close/frames/rgb/', [40 20 120 160]);
+
+%%
+MSub = M(1:30, :);
+[U S V] = svd(MSub);
+r = 4;
+d_fr = S(:, 1:r);
+d_rn = S(1:r, :);
+
+%%
+L_tilde = U*sqrt(d_fr);
+S_tilde = sqrt(d_rn)*V;
+S_tilde = bsxfun(@rdivide, S_tilde, sqrt(sum(S_tilde.*S_tilde, 2)));
