df808b9c7e44f1938e3f36d762cedf50245981d9
themiller <themiller91@gmail.com>
2013-12-08 22:22:02 -0800

    Added local fitting

--- code/photometric_stereo.py
+++ code/photometric_stereo.py
@@ -86,7 +86,7 @@ def integrate_normals(normals, mask):
                     b[row_idx2] = ny;
 
     A = scipy.sparse.csr_matrix(A)
-    z, istop, itmax, r1norm = scipy.sparse.linalg.lsqr(A, b)[0:4]
+    z, istop, itmax, r1norm = scipy.sparse.linalg.lsmr(A, b)[0:4]
     
     depth = numpy.zeros((M, N))
     for i in range(M):
@@ -98,7 +98,7 @@ def integrate_normals(normals, mask):
     return depth
 
 def make_M(image_filenames, roi):
-    gamma =1.0/2.2
+    gamma = 1.0/2.2
     _, _, width, height = roi
     M = numpy.zeros((len(image_filenames), width*height))
     for (i, image_filename) in enumerate(image_filenames):
@@ -146,6 +146,17 @@ def solve_bas_relief(ref_depth, depth, valid):
     t, _, _, _ = numpy.linalg.lstsq(A, b)
     return t
 
+def fit_local_model(M, L, N):
+    for i in range(M.shape[1]):
+        errors = numpy.sqrt((M[:,i] - numpy.dot(L,N[:,i]))**2)
+        max_error = numpy.max(errors)
+        if max_error > 1e-5:
+            rel_error = errors / max_error
+            good_obs = numpy.logical_and(numpy.logical_and(rel_error < 0.1, M[:,i] > 0.25), M[:,i] < 0.8)
+            if numpy.sum(good_obs) >= 5:
+                N_i, _, _, _ = numpy.linalg.lstsq(L[good_obs,:], M[good_obs,i])
+                N[:,i] = N_i
+
 
 def main():
     video_dir = r'C:\Projects\GitHub\rgbd-photometric\rgbd-util\out'
@@ -157,7 +168,7 @@ def main():
     depth_image_filenames = depth_image_filenames[0:10]
 
     im = cv2.imread(depth_image_filenames[0], -1)
-    im[im>420] = 0
+    im[im>400] = 0
     width = 80
     height = 160
     roi = [50, 20, width, height]
@@ -174,6 +185,7 @@ def main():
 
     M = make_M(rgb_image_filenames, roi)
     L, N = solve_for_L_and_N(M,3)
+    fit_local_model(M, L, N)
     # normals_image = numpy.zeros((height, width, 3))
     # for i in range(height):
     #     for j in range(width):
@@ -197,16 +209,20 @@ def main():
     depth = integrate_normals(normals_image, numpy.ones((height, width)))
 
     t = solve_bas_relief(pcloud[:,:,2], depth, pcloud[:,:,2]>0)
+    #t = solve_bas_relief(pcloud[:,:,2], depth, valid)
 
     indices = -1*numpy.ones((height, width), numpy.int32)
     out = open('test.obj', 'w')
+    out2 = open('depth.obj', 'w')
     count = 0
     for i in range(height):
         for j in range(width):
-            #valid[i,j] = numpy.linalg.norm(normals_image[i,j,:]) > 0.01
+            x, y = pcloud[i,j,0:2]
+            valid[i,j] = numpy.linalg.norm(normals_image[i,j,:]) > 0.01
             if valid[i,j]:
-                #out.write('v {0} {1} {2}\n'.format(j, -i,-(t[0]*j + t[1]*i + t[2]*depth[i,j] + t[3])))
-                out.write('v {0} {1} {2}\n'.format(j, -i,-pcloud[i,j,2]))
+                out.write('v {0} {1} {2}\n'.format(j, -i,-(t[0]*j + t[1]*i + t[2]*depth[i,j] + t[3])))
+                out2.write('v {0} {1} {2}\n'.format(j, -i,-pcloud[i,j,2]))
+                #out.write('v {0} {1} {2}\n'.format(pcloud[i,j,0], pcloud[i,j,1],-pcloud[i,j,2]))
                 indices[i,j] = count
                 count += 1
 
